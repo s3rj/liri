ccd0775ff92b172090265da74786ddf6588db78e
serj <astralninja@gmail.com>
2018-02-19 21:49:57 -0600

    dependencies required

--- liri.js
+++ liri.js
@@ -1,12 +1,11 @@
 
-
+// node packages
 require("dotenv").config();
 
-
 var Spotify = require("node-spotify-api");
-
-require("request");
 var Twitter = require("twitter");
+
+var request = require("request");
 
 //import keys.js
 keys = require("./keys.js");
@@ -14,10 +13,14 @@ keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
+//cl arguments
+var args = process.argv;
+command = args[2];
+console.log(args);
 
-//console.log(spotify)
-//console.log(client)
 
+
+//twitter stuff
 var params = {screen_name: 'vsambros'};
 client.get('statuses/user_timeline', params, function(error, tweets, response) {
   if (!error) {
@@ -25,11 +28,24 @@ client.get('statuses/user_timeline', params, function(error, tweets, response) {
   }
 });
 
-
+//spotify stuff here 
 spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
   if (err) {
     return console.log('Error occurred: ' + err);
   }
  
-console.log(JSON.stringify(data.tracks,null,4)); 
+
+
+
+console.log(JSON.stringify(data.tracks.items[0].name,null,3)); 
 });
+
+switch(command){
+	case "tweets":
+		console.log("print tweets");
+		break;
+	case "spotify":
+		console.log("look up song");
+	default:
+		console.log("fix your syntax");
+}
